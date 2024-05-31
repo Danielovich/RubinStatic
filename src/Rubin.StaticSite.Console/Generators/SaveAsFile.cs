85e7ff65fb797f631db7318154ce7da9904e5c40
Danielovich <daniel@zerfro.dk>
2024-05-31 13:02:09 +0200

    overload

--- src/Rubin.StaticSite.Console/Generators/SaveAsFile.cs
+++ src/Rubin.StaticSite.Console/Generators/SaveAsFile.cs
@@ -1,11 +1,8 @@
-﻿using Rubin.Static;
-using Rubin.Static.Models;
-
-namespace Rubin.Markdown.Console.Generators;
+﻿namespace Rubin.Markdown.Console.Generators;
 
 public class SaveAsFile : ISavePage
 {
-    public async Task<string> Save(string content, Slug identifier)
+    public async Task<string> Save(RenderedPage rendering)
     {
         // save it at bin/{debug|release}/{.net version}/Views/Output/*.html
         var path = string.Concat(
@@ -14,11 +11,25 @@ public class SaveAsFile : ISavePage
             Path.DirectorySeparatorChar,
             "Output",
             Path.DirectorySeparatorChar,
-            identifier.Instance,
+            rendering.Slug.Instance,
             ".html");
 
-        await File.WriteAllTextAsync(path, content);
+        await File.WriteAllTextAsync(path, rendering.Content);
 
         return path;
+    }
+
+    public async Task<IEnumerable<string>> Save(IEnumerable<RenderedPage> renderings)
+    {
+        var result = new List<string>();
+
+        foreach (var item in renderings)
+        {
+            result.Add(
+                await Save(item)
+            );
+        }
+
+        return result;
     }
 }
