3c377249de6a7f22df119df90be3a8016ce261b8
Danielovich <daniel@zerfro.dk>
2024-05-31 13:01:18 +0200

    congested calls and removed usings (not sure this was a great idea!)

--- src/Rubin.StaticSite.Console/Extensions/ServiceCollectionExtensions.cs
+++ src/Rubin.StaticSite.Console/Extensions/ServiceCollectionExtensions.cs
@@ -1,47 +1,35 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Rubin.Markdown.Console.Generators;
-using Rubin.Markdown.Console.Mappers;
-using Rubin.Markdown.Extensions;
-using Rubin.Static.Infrastructure;
-using Rubin.Static.Services;
-using System.CommandLine;
+﻿namespace Rubin.Markdown.Console.Extensions;
 
-namespace Rubin.Markdown.Console.Extensions
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static void Setup(this IServiceCollection services)
     {
-        public static void Setup(this IServiceCollection services)
+        // configure logging
+        services.AddLogging(builder =>
         {
-            // configure logging
-            services.AddLogging(builder =>
-            {
-                builder.AddConsole();
-                builder.AddDebug();
-            });
+            builder.AddConsole();
+            builder.AddDebug();
+        });
 
-            // build config
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false)
-                .Build();
+        // build config
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
 
-            services.AddScoped<IConfiguration>(_ => configuration);
-            services.AddTransient<IMapToPosts, PostService>();
-            services.AddTransient<IGeneratePages, PageGenerator>();
-            services.AddTransient<ISavePage, SaveAsFile>();
+        services.AddScoped<IConfiguration>(_ => configuration);
+        services.AddTransient<IMapToPosts, PostService>();
+        services.AddTransient<IGeneratePages, PageGenerator>();
+        services.AddTransient<ISavePage, SaveAsFile>();
 
-            services.AddMarkdown();
-            services.AddRazorTemplating();
-            services.AddStatic();
+        services.AddMarkdown();
+        services.AddStatic();
 
-            // Building provider due to the nature of how the default Renderer works
-            // Not favourable, I know!
-            var renderer = new Renderer(services.BuildServiceProvider());
-            services.AddSingleton<IRenderer>(_ => renderer);
+        // Building provider due to the nature of how the default Renderer works
+        // Not favourable, I know!
+        var renderer = new Renderer(services.BuildServiceProvider());
+        services.AddSingleton<IRenderer>(_ => renderer);
 
-            services.AddTransient<Command, GenerateCommand>();
-        }
+        services.AddTransient<Command, GenerateCommand>();
     }
 }
