87ba3ff81a8661102d6994166004230b284c7274
Danielovich <daniel@zerfro.dk>
2024-05-31 13:00:26 +0200

    extensions for a few specialised cases around category posts

---
+++ src/Rubin.StaticSite.Console/Extensions/CategoryPostExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Rubin.Static.Console;
+public static class CategoryPostExtensions
+{
+    public static Dictionary<Category, IEnumerable<Post>> OrderByPublishDate(this Dictionary<Category, IEnumerable<Post>> posts)
+    {
+        var orderedDict = posts.ToDictionary(
+            pair => pair.Key,
+            pair => pair.Value.OrderByDescending(post => post.PublishedDate).AsEnumerable());
+
+        return orderedDict;
+    }
+
+    public static IEnumerable<Post> OrderByPublishDate(this IEnumerable<Post> posts)
+    {
+        var orderedPosts = posts.OrderByDescending(post => post.PublishedDate).AsEnumerable();
+
+        return orderedPosts;
+    }
+}
