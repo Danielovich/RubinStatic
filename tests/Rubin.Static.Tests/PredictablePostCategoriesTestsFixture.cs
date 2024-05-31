e4f602029e664135f26cd6e27b44990a97c40b11
Danielovich <daniel@zerfro.dk>
2024-05-31 13:07:04 +0200

    renamed test files

---
+++ tests/Rubin.Static.Tests/PredictablePostCategoriesTestsFixture.cs
@@ -0,0 +1,30 @@
+﻿namespace Rubin.Static.Tests;
+
+/// <summary>
+/// This type is for making testing Post Categories easier.
+/// Categories can be "set" on a Post and here we set a random number of categories 
+/// </summary>
+public class PredictablePostCategoriesTestsFixture
+{
+    public Fixture Fixture { get; private set; }
+
+    public PredictablePostCategoriesTestsFixture()
+    {
+        Fixture = new Fixture();
+
+        // customize fixture since we need each post to have a predictable number and names of categories 
+        Fixture.Customize<IEnumerable<Category>>(p => p.FromFactory(() =>
+        {
+            return PostCategoriesCustomization.RandomPickedCategories("working, personal,   code, food, professional");
+        }));
+        
+        Fixture.Customize<Post>(c => c.FromFactory(() => new Post(
+            Fixture.Create<Title>(),
+            Fixture.Create<HtmlContent>(),
+            Fixture.Create<IEnumerable<Category>>(), //this will use the Category customization
+            Fixture.Create<Slug>(),
+            Fixture.Create<DateTime>(),
+            true
+        )));
+    }
+}
