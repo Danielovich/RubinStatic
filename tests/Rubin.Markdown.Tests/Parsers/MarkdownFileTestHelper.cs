8ab7e96175935be0f735afba5daabb3cda1350c0
Danielovich <daniel@zerfro.dk>
2024-06-04 12:13:19 +0200

    another assets to Assets

--- tests/Rubin.Markdown.Tests/Parsers/MarkdownFileTestHelper.cs
+++ tests/Rubin.Markdown.Tests/Parsers/MarkdownFileTestHelper.cs
@@ -6,7 +6,7 @@ public static class MarkdownFileTestHelper
 {
     public static List<MardownFile> MarkdownFileCollection()
     {
-        var markdownFile = Path.Combine(Environment.CurrentDirectory, "assets", "post.md");
+        var markdownFile = Path.Combine(Environment.CurrentDirectory, "Assets", "post.md");
 
         List<MardownFile> markdownFiles = new();
         markdownFiles.Add(new MardownFile() { Contents = File.ReadAllText(markdownFile) });
